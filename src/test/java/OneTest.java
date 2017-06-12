7a57d4fe807b7b9b7f7a0e1a485049d37595550f
Bouzidi Bouaza <bouzidi.bouaza@gmail.com>
2017-06-12 13:19:55 +0000

    Create OneTest.java
    change3

--- src/test/java/OneTest.java
+++ src/test/java/OneTest.java
@@ -27,10 +27,10 @@ public class OneTest {
 		assertEquals("the result must be true", true, contactDAO.updateContact(11, "karabila", "Mohamed", "0643207443", "tester", "tester@gmail.com"));
 	}
 	
-	@Test
+	/*@Test
 	public void deleteContactTest(){
 		assertEquals("the result must be true", true, contactDAO.deleteContact(1));
-	}
+	}*/
 	
 	@Test
 	public void getContactByIdTest(){
