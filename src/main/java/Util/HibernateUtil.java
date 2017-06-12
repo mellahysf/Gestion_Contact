fc28bc70ba6bf5b1884f51b771ff3efa2e17a1ae
Bouzidi Bouaza <bouzidi.bouaza@gmail.com>
2017-06-12 12:00:52 +0000

    version final 1

--- src/main/java/Util/HibernateUtil.java
+++ src/main/java/Util/HibernateUtil.java
@@ -2,7 +2,7 @@ package Util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+// hibernate.util
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory=buildSessionFactory();
 	private static SessionFactory buildSessionFactory(){
